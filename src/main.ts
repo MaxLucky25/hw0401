b447d0e613f73df91b3af96763a68b684cfc29db
MaksLucky25 <maksrad93@gmail.com>
2025-06-12 13:07:48 +0300

    change rootDir

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,4 @@
 import { NestFactory } from '@nestjs/core';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
@@ -7,15 +6,6 @@ const PORT = process.env.PORT || 3003;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  const config = new DocumentBuilder()
-    .setTitle('Blogger-Platform')
-    .setDescription('API description')
-    .setVersion('1.0')
-    .addTag('Bloggers')
-    .build();
-  const documentFactory = () => SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, documentFactory);
 
   app.enableCors();
   app.useGlobalPipes(
